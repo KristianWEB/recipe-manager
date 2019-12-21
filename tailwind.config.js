5bf4146a11b9c44367d808c18acab0f17e2fbdae
Kristian <devWEBpk@outlook.com>
2019-12-21 17:39:04 +0200

    implemented login form and page, fixed a few styling issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
                 orange: "#f35627",
                 "dark-gray": "#616e7c",
                 "light-gray": "#f1f5f8",
-                "input-gray": "#7b8793"
+                "input-gray": "#7b8793",
+                "heading-primary": "#1F2933"
             }
         }
     },
