d1bf64fca4748f64b2a32403e4f91b4e220ae29f
Kristian <devWEBpk@outlook.com>
2020-01-20 18:21:05 +0200

    successfully implemented saving functionality

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -9,12 +9,13 @@ class RecipeController extends Controller
 {
     public function store()
     {
-        // validate
-        $attributes = request()->all();
+        $attributes['image'] = request()->input('image');
+        $attributes['label'] = request()->input('label');
+        $attributes['ingredients'] = json_encode(request()->input('ingredients'));
+        $attributes['calories'] = request()->input('calories');
+        $attributes['weight'] = request()->input('totalWeight');
+        $attributes['source_url'] = request()->input('url');
 
-        // $attributes['ingredients'] = json_encode($attributes['ingredients']);
-
-        // auth()->user()->recipes()->create($attributes);
-        return $attributes;
-    }
+        auth()->user()->recipes()->create($attributes);
+    } // 
 }
