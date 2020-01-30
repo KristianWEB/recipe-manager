0a3d432fd961f32e783a7a98279ddfa435817f3e
Kristian <46797006+KristianWEB@users.noreply.github.com>
2020-01-30 20:28:18 +0200

    detailed searching implemented (#44)

--- database/migrations/2019_08_25_155533_create_recipes_table.php
+++ database/migrations/2019_08_25_155533_create_recipes_table.php
@@ -18,7 +18,7 @@ class CreateRecipesTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->string('image');
             $table->string('label');
-            $table->json('ingredients');
+            $table->text('ingredients');
             $table->integer('calories');
             $table->integer('weight');
             $table->string('url');
