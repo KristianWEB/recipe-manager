448a613796e63be11666c8f11e2ca55e50864925
Kristian <devWEBpk@outlook.com>
2020-03-08 00:30:24 +0200

    removed .storybook folder

--- .storybook/config.js
+++
@@ -1,4 +0,0 @@
-import { configure } from "@storybook/vue";
-import "./utils.css";
-
-configure(require.context("../resources", true, /\.stories\.js$/), module);
--- .storybook/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const tailwindcss = require("tailwindcss");
-
-module.exports = {
-    plugins: [
-        require("postcss-import")(),
-        tailwindcss("./tailwind.config.js"),
-        require("autoprefixer")
-    ]
-};
